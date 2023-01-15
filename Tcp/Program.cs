e32b1b7405576c88f5c2cb9b87229098f8948810
Onuraslann <theeaslann1@gmail.com>
2023-01-15 19:10:03 +0300

    Add project files.

---
+++ Tcp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Tcp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+          
+            TcpHelper.StartServer(9000);
+            TcpHelper.Listen(); // Start listening
+        }
+    }
+}
